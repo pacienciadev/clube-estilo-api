baeaa9c65b0f388edf630ac28dccaac3224eaa1f
Rafael Paciencia <rafapaci@gmail.com>
2025-09-02 22:23:58 -0300

    chore: Config cors API_URL env option

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: process.env.API_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
