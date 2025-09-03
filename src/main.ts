fcb4d3f19d2b3c5c19a29b7adb4c2d0ecd0b1c73
Rafael Paciencia <rafapaci@gmail.com>
2025-09-02 23:32:16 -0300

    chore: Fix last changes

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.API_URL,
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   });
 
